a6072ede7e47ee2b0ff6662b3677299d51a178b2
Emily Winnink <emily.winnink@gmail.com>
2025-04-02 17:35:02 +0200

    Added randomizer for ints

--- inc/rand.hpp
+++ inc/rand.hpp
@@ -10,4 +10,8 @@ namespace erand
 	std::uniform_real_distribution<double>	generator(const double f1, const double f2);
 
 	double	rand(std::uniform_real_distribution<double> &gen);
+
+	std::uniform_int_distribution<int>	generator(const int n1, const int n2);
+
+	int	rand(std::uniform_int_distribution<int> &gen);
 }
